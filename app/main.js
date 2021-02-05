378fabd607aa8d14f8a7f1b155acc2fd1c11f99d
Aapo Alasuutari <aapo.alasuutari@valmet.com>
2021-02-05 19:13:14 +0200

    Optimize, timeout non-finishing cache match Promises on SW side

--- app/main.js
+++ app/main.js
@@ -1,62 +1,63 @@
 // Basics
 
 const jQuery = function (el) {
-    return new jQuery(el);
-}
-
-const assignIn = (child, parent) => {
-    for (const ownOrInheritedKey in parent) {
-        child[ownOrInheritedKey] = parent[ownOrInheritedKey];
-    }
+  return new jQuery(el);
 };
 
-window.providedData = class ProvidedData { 
-    static extend(protoProps) {
-        const parent = this;
-        let child;
+const assignIn = (child, parent) => {
+  for (const ownOrInheritedKey in parent) {
+    child[ownOrInheritedKey] = parent[ownOrInheritedKey];
+  }
+};
 
-        if (protoProps && protoProps.hasOwnProperty(constructor)) {
-            child = protoProps.constructor;
+window.providedData = class ProvidedData {
+  static extend(protoProps) {
+    const parent = this;
+    let child;
 
-            const Surrogate = function () {
-                this.constructor = child;
-            };
-            Surrogate.prototype = parent.prototype;
-            child.prototype = new Surrogate();
+    if (protoProps && protoProps.hasOwnProperty(constructor)) {
+      child = protoProps.constructor;
 
-            child.__super__ = parent.prototype;
-        } else {
-            child = class extends parent { };
-        }
+      const Surrogate = function () {
+        this.constructor = child;
+      };
+      Surrogate.prototype = parent.prototype;
+      child.prototype = new Surrogate();
 
-        if (protoProps) {
-            assignIn(child.prototype, protoProps);
-        }
-
-        return child;
+      child.__super__ = parent.prototype;
+    } else {
+      child = class extends parent {};
     }
+
+    if (protoProps) {
+      assignIn(child.prototype, protoProps);
+    }
+
+    return child;
+  }
 };
 window.$ = jQuery;
 
 // Dynamic imports without CSP violations, see https://bugs.chromium.org/p/chromium/issues/detail?id=979351
-const load = url => new Promise((res, rej) => {
+const load = (url) =>
+  new Promise((res, rej) => {
     const script = document.createElement("script");
     script.url = url;
     script.type = "module";
     document.head.appendChild(script);
     const timeoutId = setTimeout(() => {
-        script.remove();
-        rej(new Error("timeout"));
+      script.remove();
+      rej(new Error("timeout"));
     }, 10000);
-    import(url).then(module => {
-        clearTimeout(timeoutId);
-        script.remove();
-        res(module);
-    }).catch(err => {
-        script.remove();
-        rej(err);
+    import(url).then((module) => {
+      clearTimeout(timeoutId);
+      script.remove();
+      res(module);
+    }).catch((err) => {
+      script.remove();
+      rej(err);
     });
-});
+  });
 
 // Dynamic style imports
 const STYLE_IMPORTER_SELECTOR = "#import-sheet";
@@ -66,155 +67,157 @@ let importSheet;
 const importedStylesheets = new Set();
 const loadingStylesheets = new Map();
 
-const getRuleForUrl = (stylesheetUrl) =>
-    `@import url("${stylesheetUrl}");`;
+const getRuleForUrl = (stylesheetUrl) => `@import url("${stylesheetUrl}");`;
 
 const manuallyCheckResolves = () =>
-    setTimeout(() => {
-        if (loadingStylesheets.size > 0) {
-            resolveImportPromise();
-            if (loadingStylesheets.size > 0) {
-                manuallyCheckResolves();
-            }
-        }
-    }, 300);
+  setTimeout(() => {
+    if (loadingStylesheets.size > 0) {
+      resolveImportPromise();
+      if (loadingStylesheets.size > 0) {
+        manuallyCheckResolves();
+      }
+    }
+  }, 300);
 
 const commitStylesheet = (stylesheetUrl) => {
-    importSheet.insertRule(getRuleForUrl(stylesheetUrl));
-    importedStylesheets.add(stylesheetUrl);
-    manuallyCheckResolves();
-    return new Promise(res => {
-        const timeoutId = setTimeout(() => revertStylesheet(stylesheetUrl), 10000);
-        loadingStylesheets.set(stylesheetUrl, () => {
-            clearTimeout(timeoutId);
-            loadingStylesheets.delete(stylesheetUrl);
-            res();
-        });
+  importSheet.insertRule(getRuleForUrl(stylesheetUrl));
+  importedStylesheets.add(stylesheetUrl);
+  manuallyCheckResolves();
+  return new Promise((res) => {
+    const timeoutId = setTimeout(() => revertStylesheet(stylesheetUrl), 10000);
+    loadingStylesheets.set(stylesheetUrl, () => {
+      clearTimeout(timeoutId);
+      loadingStylesheets.delete(stylesheetUrl);
+      res();
     });
+  });
 };
 
 const resolveImportPromise = () => {
-    if (loadingStylesheets.size === 0) {
-        return;
-    }
-    const importedStylesheetUrls = Array.from(importSheet.cssRules).filter(
-        (cssRule) =>
-            cssRule instanceof CSSImportRule &&
-            loadingStylesheets.has(cssRule.href) &&
-            cssRule.styleSheet instanceof CSSStyleSheet
-    );
-    for (const cssRule of importedStylesheetUrls) {
-        cssRule.styleSheet.cssRules;
-        const resolveFunction = loadingStylesheets.get(cssRule.href);
-        resolveFunction();
-    }
+  if (loadingStylesheets.size === 0) {
+    return;
+  }
+  const importedStylesheetUrls = Array.from(importSheet.cssRules).filter(
+    (cssRule) =>
+      cssRule instanceof CSSImportRule &&
+      loadingStylesheets.has(cssRule.href) &&
+      cssRule.styleSheet instanceof CSSStyleSheet,
+  );
+  for (const cssRule of importedStylesheetUrls) {
+    cssRule.styleSheet.cssRules;
+    const resolveFunction = loadingStylesheets.get(cssRule.href);
+    resolveFunction();
+  }
 };
 
 const revertStylesheet = (stylesheetUrl) => {
-    if (loadingStylesheets.has(stylesheetUrl)) {
-        // Resolve the loading promise
-        loadingStylesheets.get(stylesheetUrl)();
+  if (loadingStylesheets.has(stylesheetUrl)) {
+    // Resolve the loading promise
+    loadingStylesheets.get(stylesheetUrl)();
+  }
+  try {
+    const ruleString = getRuleForUrl(stylesheetUrl);
+    const deleteIndex = Array.prototype.findIndex.call(
+      importSheet.rules,
+      (rule) => rule.cssText === ruleString,
+    );
+    if (deleteIndex !== -1) {
+      importSheet.deleteRule(deleteIndex);
+      return importedStylesheets.delete(stylesheetUrl);
     }
-    try {
-        const ruleString = getRuleForUrl(stylesheetUrl);
-        const deleteIndex = Array.prototype.findIndex.call(
-            importSheet.rules,
-            (rule) => rule.cssText === ruleString
-        );
-        if (deleteIndex !== -1) {
-            importSheet.deleteRule(deleteIndex);
-            return importedStylesheets.delete(stylesheetUrl);
-        }
-        return false;
-    } catch (err) {
-        console.log("Failed to revert stylesheet import", err);
-        return false;
-    }
+    return false;
+  } catch (err) {
+    console.log("Failed to revert stylesheet import", err);
+    return false;
+  }
 };
 
 const tryGetImportSheet = () => {
-    if (importSheet) {
+  if (importSheet) {
+    return true;
+  }
+  const importStyleElement = document.querySelector(STYLE_IMPORTER_SELECTOR);
+  if (importStyleElement) {
+    for (const stylesheet of document.styleSheets) {
+      if (stylesheet.ownerNode === importStyleElement) {
+        importSheet = stylesheet;
+        importStyleElement.addEventListener(
+          "load",
+          resolveImportPromise,
+          { passive: true },
+        );
+        importStyleElement.addEventListener(
+          "error",
+          resolveImportPromise,
+          { passive: true },
+        );
         return true;
+      }
     }
-    const importStyleElement = document.querySelector(STYLE_IMPORTER_SELECTOR);
-    if (importStyleElement) {
-        for (const stylesheet of document.styleSheets) {
-            if (stylesheet.ownerNode === importStyleElement) {
-                importSheet = stylesheet;
-                importStyleElement.addEventListener(
-                    "load",
-                    resolveImportPromise,
-                    { passive: true }
-                );
-                importStyleElement.addEventListener(
-                    "error",
-                    resolveImportPromise,
-                    { passive: true }
-                );
-                return true;
-            }
-        }
-    }
-    return false;
+  }
+  return false;
 };
 
 const addStylesheet = (stylesheetUrl) => {
-    if (importedStylesheets.has(stylesheetUrl)) {
-        return Promise.resolve();
-    } else if (loadingStylesheets.has(stylesheetUrl)) {
-        // Daisy-chain into the original resolve function.
-        const originalResolve = loadingStylesheets.get(stylesheetUrl);
-        return new Promise(res => {
-            loadingStylesheets.set(stylesheetUrl, () => {
-                originalResolve();
-                res();
-            });
-        });
-    }
-    if (tryGetImportSheet()) {
-        return commitStylesheet(stylesheetUrl);
-    } else {
-        // Couldn't find import sheet
-        return Promise.reject(
-            new TypeError(
-                "Invalid style-importer usage, import stylesheet not detected"
-            )
-        );
-    }
+  if (importedStylesheets.has(stylesheetUrl)) {
+    return Promise.resolve();
+  } else if (loadingStylesheets.has(stylesheetUrl)) {
+    // Daisy-chain into the original resolve function.
+    const originalResolve = loadingStylesheets.get(stylesheetUrl);
+    return new Promise((res) => {
+      loadingStylesheets.set(stylesheetUrl, () => {
+        originalResolve();
+        res();
+      });
+    });
+  }
+  if (tryGetImportSheet()) {
+    return commitStylesheet(stylesheetUrl);
+  } else {
+    // Couldn't find import sheet
+    return Promise.reject(
+      new TypeError(
+        "Invalid style-importer usage, import stylesheet not detected",
+      ),
+    );
+  }
 };
 
 window.addStylesheet = addStylesheet;
 
 // Main
 const main = async () => {
-    const registration = await navigator.serviceWorker.register("./worker.js");
-    await navigator.serviceWorker.ready;
-    if ("sync" in registration) {
-        await registration.sync.register("sync");
-    } else {
-        await registration.active.postMessage("sync");
-    }
+  const registration = await navigator.serviceWorker.register("./worker.js");
+  await navigator.serviceWorker.ready;
+  if ("sync" in registration) {
+    await registration.sync.register("sync");
+  } else {
+    await registration.active.postMessage("sync");
+  }
 
-    const loadButton = document.querySelector(".load-button");
-    loadButton.addEventListener("click", async () => {
-        const { default: { data, stylesheets } } = await load("/entries/bar.js").catch(err => {
-            const errordiv = document.createElement("div");
-            errordiv.textContent = "ERROR OCCURRED: " + err.message;
-            document.body.appendChild(errordiv);
-            throw err;
-        });
-        const promises = stylesheets.map(addStylesheet);
-        await Promise.all(promises).catch(err => { /* disregard */ });
-        const { default: { data: data2 }} = await load("/entries/myProvidedData.js");
-    
-        const dataEl = document.createElement("div");
-        dataEl.className = "load-result";
-        dataEl.textContent = `
+  const loadButton = document.querySelector(".load-button");
+  loadButton.addEventListener("click", async () => {
+    const { default: { data, stylesheets } } = await load("/entries/bar.js")
+      .catch((err) => {
+        const errordiv = document.createElement("div");
+        errordiv.textContent = "ERROR OCCURRED: " + err.message;
+        document.body.appendChild(errordiv);
+        throw err;
+      });
+    const promises = stylesheets.map(addStylesheet);
+    await Promise.all(promises).catch((err) => {/* disregard */});
+    const { default: { data: data2 } } = await load(
+      "/entries/myProvidedData.js",
+    );
+
+    const dataEl = document.createElement("div");
+    dataEl.className = "load-result";
+    dataEl.textContent = `
         ${data.toString()}
         ${stylesheets}
         ${data2.toString()}`;
-        document.body.appendChild(dataEl);
-    });
-}
+    document.body.appendChild(dataEl);
+  });
+};
 
 main();
--- app/worker.js
+++ app/worker.js
@@ -21,7 +21,7 @@ self.addEventListener("sync", event =>
     )
 );
 
-self.addEventListener("message", event => 
+self.addEventListener("message", event =>
     event.waitUntil([
         clients.claim(),
         caches.delete(RESOURCES),
@@ -58,7 +58,8 @@ const createResponseFromAttachment = (
     etag
 ) => responseFromBuffer(utf8base64ToUint8Array(data), contentType, etag);
 
-const handleEntryRequest = async event => {
+const handleEntryRequest = async (event) => {
+    const cache = await caches.open(RESOURCES);
     const base = event.request.url.substring(event.request.url.indexOf("/entries/"));
     const nextSeparator = base.indexOf("/", 9);
     const nextDot = base.indexOf(".", 9);
@@ -70,20 +71,52 @@ const handleEntryRequest = async event => {
     } else if (nextDot !== -1) {
         endIndex = nextDot;
     }
-    const response = await fetch(`${base.substring(0, endIndex).replace("/entries/", "./resources/")}.json`);
-    const data = await response.json();
-    for (const requestUrl in data) {
-        const contentType = requestUrl.endsWith(".css") ? "text/css" : "application/javascript";
-        await caches.open(RESOURCES).then(cache => cache.put(requestUrl, createResponseFromAttachment({
-            data: data[requestUrl],
-            contentType,
-        }, response.headers.get("ETag"))));
+
+    const matchedRequest = await cache.match(base);
+    if (matchedRequest) {
+        return matchedRequest;
     }
 
-    return caches.open(RESOURCES).then(cache => cache.match(base)).then(resp => resp || new Response(undefined, { status: 404 }));
+    const response = await fetch(`${base.substring(0, endIndex).replace("/entries/", "./resources/")}.json`);
+    const data = await response.json();
+    const promises = [];
+    for (const requestUrl in data) {
+        const contentType = requestUrl.endsWith(".css")
+            ? "text/css"
+            : "application/javascript";
+        promises.push(cache.put(
+            requestUrl,
+            createResponseFromAttachment({
+                data: data[requestUrl],
+                contentType,
+            }, response.headers.get("ETag")),
+        )
+        );
+    }
+    await Promise.all(promises);
+
+    return cache.match(base).then(
+        async (resp) => {
+            if (resp) {
+                const contentType = resp.headers.get("Content-Type");
+                const timeoutPromise = new Promise((_res, rej) => setTimeout(rej, 5000));
+                const bufferPromise = resp.arrayBuffer();
+                try {
+                    const buffer = await Promise.race([
+                        bufferPromise,
+                        timeoutPromise,
+                    ]);
+                    return responseFromBuffer(new Uint8Array(buffer), contentType);
+                } catch (_nothing) {
+                    return new Response(undefined, { status: 500 });
+                }
+            }
+            return new Response(undefined, { status: 404 });
+        }
+    );
 };
 
-self.addEventListener("fetch", event => {
+self.addEventListener("fetch", (event) => {
     if (event.request.url.includes("/entries/")) {
         return event.respondWith(handleEntryRequest(event));
     } else if (event.request.url.endsWith("/main.js") || event.request.url.endsWith("/index.html")) {
